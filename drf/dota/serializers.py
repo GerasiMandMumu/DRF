31adbed3a1cfeaf28d66a3d824880afffbe10697
GerasiMandMumu <konst.02@yandex.ru>
2023-04-19 18:38:06 +0300

    TASK 2. Развитие проекта. Создание базового класса для представлений; сериализация

--- drf/dota/serializers.py
+++ drf/dota/serializers.py
@@ -1,9 +1,34 @@
+import io
+
 from rest_framework import serializers
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
 
 from dota.models import Hero
 
+# class HeroModel:
+#     def __init__(self, title, content):
+#         self.title = title
+#         self.content = content
 
-class HeroSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Hero
-        fields = ('title', 'cat_id')
+class HeroSerializer(serializers.Serializer):
+    title = serializers.CharField(max_length=255)
+    content = serializers.CharField()
+    time_create = serializers.DateTimeField()
+    time_update = serializers.DateTimeField()
+    cat_id = serializers.IntegerField()
+
+
+# def encode():
+#     model = HeroModel('Antimage', 'Content: Antimage')
+#     model_sr = HeroSerializer(model)
+#     print(model_sr.data, type(model_sr.data), sep='\n')
+#     json = JSONRenderer().render(model_sr.data)
+#     print(json)
+#
+# def decode():
+#     stream = io.BytesIO(b'{"title":"Antimage","content":"Content: Antimage"}')
+#     data = JSONParser().parse(stream)
+#     serializer = HeroSerializer(data=data)
+#     serializer.is_valid()
+#     print(serializer.validated_data)
--- drf/dota/views.py
+++ drf/dota/views.py
@@ -1,11 +1,30 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from dota.models import Hero
 
 from dota.serializers import HeroSerializer
 
 
-class HeroAPIView(generics.ListAPIView):
-    queryset = Hero.objects.all()
-    serializer_class = HeroSerializer
+class HeroAPIView(APIView):
+    def get(self, request):
+        lst = Hero.objects.all()
+        return Response({'posts': HeroSerializer(lst, many=True).data})
+
+    def post(self, request):
+        serializer = HeroSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        post_new = Hero.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': HeroSerializer(post_new).data})
+
+# class HeroAPIView(generics.ListAPIView):
+#     queryset = Hero.objects.all()
+#     serializer_class = HeroSerializer
